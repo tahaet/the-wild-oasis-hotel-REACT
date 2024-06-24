97713c777faeb355f41f078eeeb59822caa4193c
Taha <taha100_100@yahoo.com>
2024-06-24 03:19:46 +0300

    fix import bug in HeaderMenu.jsx file

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
+import Logout from "../features/authentication/logout";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 import DarkModeToggle from "./DarkModeToggle";
